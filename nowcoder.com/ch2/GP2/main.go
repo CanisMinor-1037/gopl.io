faeda39894789e7974df1213ce0869dffb71587f
CanisMinor <canisminor@qq.com>
2024-12-25 15:33:14 +0800

    nowcoder.com

---
+++ nowcoder.com/ch2/GP2/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "fmt"
+)
+func main() {
+  var name string = "小明"
+  var age int = 23
+  var sex bool = true
+  fmt.Println(name)
+  fmt.Println(age)
+  fmt.Println(sex)
+}
