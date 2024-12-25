ed04176f23d7658c7a4d854c0790cd70c60787fd
CanisMinor <canisminor@qq.com>
2024-12-25 11:01:01 +0800

    1

--- ch2/lengthconv/conv.go
+++
@@ -1,9 +0,0 @@
-package lengthconv
-
-func FToM(f Foot) Meter {
-	return Meter(f / 3.2808)
-}
-
-func MToF(m Meter) Foot {
-	return Foot(m * 3.2808)
-}
--- ch2/tempconv0/celsius.go
+++ ch2/tempconv0/celsius.go
@@ -1,9 +1,10 @@
 // Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
 // License: https://creativecommons.org/licenses/by-nc-sa/4.0/
 
+// See page 39.
 //!+
 
-// Package tempconv performs Celsius and Fahrenheit conversions.
+// Package tempconv performs Celsius and Fahrenheit temperature computations.
 package tempconv
 
 import "fmt"
@@ -17,7 +18,10 @@ const (
 	BoilingC      Celsius = 100
 )
 
-func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) }
-func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+
+func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
 
 //!-
+
+func (c Celsius) String() string { return fmt.Sprintf("%g°C", c) }
